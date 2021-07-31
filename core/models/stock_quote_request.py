f025df5b011497e1e4f5022f797b20cd9fa2ace6
Jiabo Yu <yujiabo3@gmail.com>
2021-07-31 18:40:05 -0400

    add unit test for data getters

--- core/models/stock_quote_request.py
+++ core/models/stock_quote_request.py
@@ -6,10 +6,5 @@ from .data_request import DataRequest
 @dataclass
 class StockQuoteRequest(DataRequest):
     ticker: str
-<<<<<<< HEAD
     start_date: str = None
     end_date: str = None
-=======
-    start_date: str
-    end_date: str
->>>>>>> add data getters
